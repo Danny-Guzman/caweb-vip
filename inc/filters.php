65b8cdd325e0437ecf6a1c84e798b8283833de40
Danny Guzman <guzmanjd86@gmail.com>
2022-01-21 15:05:08 -0500

    Added filter for auth_cookie_expiration set to 30 mins

--- inc/filters.php
+++ inc/filters.php
@@ -7,6 +7,7 @@
 
 /* WP Filters */
 add_filter( 'the_content', 'caweb_vip_the_content', 10 );
+add_filter( 'auth_cookie_expiration', 'caweb_vip_auth_cookie_expiration', 10, 3 );
 
 /**
  * Filters the post content adding a version query variable to any src attributes.
@@ -29,4 +30,17 @@ function caweb_vip_the_content( $output ) {
 	}
 
 	return $output;
+}
+
+/**
+ * Filters the duration of the authentication cookie expiration period.
+ *
+ * @param  int  $length Duration of the expiration period in seconds.
+ * @param  int  $user_id User ID.
+ * @param  bool $remember Whether to remember the user login. Default false.
+ * @return int
+ */
+function caweb_vip_auth_cookie_expiration( $length, $user_id, $remember ) {
+	// 60 seconds * 30 mins
+	return 60 * 30;
 }
