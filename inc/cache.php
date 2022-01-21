a5629747eb5273c1e4e66926ad224a00be8487dd
Danny Guzman <guzmanjd86@gmail.com>
2022-01-21 17:11:33 -0500

    Fixed cache files

--- inc/cache.php
+++ inc/cache.php
@@ -22,20 +22,20 @@ add_action( 'update_option_ca_custom_js', 'caweb_vip_clear_caweb_external_js_cac
  *
  * @return string
  */
-function caweb_vip_clear_caweb_external_css_cache( $old_value, $value, $option ) {
-	if ( ! function_exists( 'wpcom_vip_purge_edge_cache_for_url' ) || ! defined( 'CAWEB_EXTERNAL_DIR' ) || ! defined( 'CAWEB_EXTERNAL_URI' ) ) {
-		return;
-	}
+function caweb_vip_clear_caweb_external_css_cache( $old_value, $value, $option ){
+    if( ! function_exists('wpcom_vip_purge_edge_cache_for_url') || ! defined('CAWEB_EXTERNAL_DIR') || ! defined('CAWEB_EXTERNAL_URI') ){
+        return;
+    }
 
-	$id  = get_current_blog_id();
-	$dir = sprintf( '%1$scss/%2$s/*.css', CAWEB_EXTERNAL_DIR, $id );
+    $id = get_current_blog_id();
+    $dir = sprintf('%1$scss/%2$s/*.css', CAWEB_EXTERNAL_DIR, $id);
 
-	foreach ( glob( $dir ) as $file ) {
-		$fn  = basename( $file );
-		$url = sprintf( '%1$s/css/%2$s/%3$s', CAWEB_EXTERNAL_URI, $id, $fn );
+    foreach( glob($dir) as $file ){
+        $fn = basename( $file );
+    	$url = sprintf('%1$s/css/%2$s/%3$s',CAWEB_EXTERNAL_URI, $id, $fn);
 
-		wpcom_vip_purge_edge_cache_for_url( $url );
-	}
+        wpcom_vip_purge_edge_cache_for_url( $url );
+    }
 }
 
 /**
@@ -49,18 +49,18 @@ function caweb_vip_clear_caweb_external_css_cache( $old_value, $value, $option )
  *
  * @return string
  */
-function caweb_vip_clear_caweb_external_js_cache( $old_value, $value, $option ) {
-	if ( ! function_exists( 'wpcom_vip_purge_edge_cache_for_url' ) || ! defined( 'CAWEB_EXTERNAL_DIR' ) || ! defined( 'CAWEB_EXTERNAL_URI' ) ) {
-		return;
-	}
+function caweb_vip_clear_caweb_external_js_cache( $old_value, $value, $option ){
+    if( ! function_exists('wpcom_vip_purge_edge_cache_for_url') || ! defined('CAWEB_EXTERNAL_DIR') || ! defined('CAWEB_EXTERNAL_URI') ){
+        return;
+    }
 
-	$id  = get_current_blog_id();
-	$dir = sprintf( '%1$sjs/%2$s/*.js', CAWEB_EXTERNAL_DIR, $id );
+    $id = get_current_blog_id();
+    $dir = sprintf('%1$sjs/%2$s/*.js', CAWEB_EXTERNAL_DIR, $id);
 
-	foreach ( glob( $dir ) as $file ) {
-		$fn  = basename( $file );
-		$url = sprintf( '%1$s/js/%2$s/%3$s', CAWEB_EXTERNAL_URI, $id, $fn );
+    foreach( glob($dir) as $file ){
+        $fn = basename( $file );
+    	$url = sprintf('%1$s/js/%2$s/%3$s',CAWEB_EXTERNAL_URI, $id, $fn);
 
-		wpcom_vip_purge_edge_cache_for_url( $url );
-	}
+        wpcom_vip_purge_edge_cache_for_url( $url );
+    }
 }
